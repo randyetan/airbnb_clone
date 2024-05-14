6f393b21ba91aad17ca223c2ed4fc2b83f7f16be
Randy <randytan089@gmail.com>
2024-05-14 17:19:48 -0500

    Created the trips page where it shows all the places you reserved to go with the dates and price. Also began implementing the feature that allows to cancel trips, although not fully functional yet.

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import useCountries from "@/app/hooks/useCountries";
-import { SafeListing, SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
@@ -12,7 +11,7 @@ import Button from "../Button";
 
 interface ListingCardProps {
     data: SafeListing;
-    reservation?: Reservation;
+    reservation?: SafeReservation;
     onAction?: (id: string) => void;
     disabled?: boolean;
     actionLabel?: string;
@@ -54,7 +53,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
         }, [reservation, data.price]);
 
         const reservationDate = useMemo(() => {
-            if(!reservation) {
+            if (!reservation) {
                 return null;
             }
 
