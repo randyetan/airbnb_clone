f55d9dc52b9fc4fa82a2c2fbfd1292bff901d6a1
Randy <randytan089@gmail.com>
2024-05-14 18:22:46 -0500

    Added favorites and properties screen.

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import getCurrentUser from "./actions/getCurrentUser";
 import Container from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";
-import getListings from "./actions/getListings";
+import getListings, { IListingParams} from "./actions/getListings";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingParams
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -46,3 +50,5 @@ export default async function Home() {
     </ClientOnly>
   )
 }
+
+export default Home;
